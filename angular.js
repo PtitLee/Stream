965987fb0954a064402d1e3ec06309226dbc4536
alexis <alexis.rouge@tagora.fr>
2015-03-11 18:36:44 +0100

    start dailymotion interaction

--- angular.js
+++ angular.js
@@ -24,10 +24,12 @@ StreamApp.controller('ContentCtrl', function ($scope, $rootScope, $sce) {
 
 StreamApp.controller('ModalCtrl', function ModalCtrl($scope, $rootScope, $sce, $rootScope, $modal, $log) {
 
-  $scope.open = function (size) {
+
+  //Gestion de la modal twitch
+  $scope.openModalTwitch = function (size) {
 
     var modalInstance = $modal.open({
-      templateUrl: 'modal_list_live_stream.html',
+      templateUrl: 'modal_live_twitch.html',
       controller: 'ModalInstanceCtrl',
       size: size,
       resolve: {
@@ -44,12 +46,37 @@ StreamApp.controller('ModalCtrl', function ModalCtrl($scope, $rootScope, $sce, $
       $log.info('Modal dismissed at: ' + new Date());
     });
   };
+
+
+  //Gestion de la modal GamingLive
+  $scope.openModalGamingLive = function (size) {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'modal_live_dailymotion.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+
+      }
+    });
+
+    modalInstance.result.then(function (stream_name) {
+
+      $rootScope.video_url = $sce.trustAsResourceUrl('http://www.twitch.tv/' + stream_name + '/embed');
+      $rootScope.chat_url = $sce.trustAsResourceUrl('http://www.twitch.tv/' + stream_name + '/chat?popout=');
+
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+
 });
 
 StreamApp.controller('ModalInstanceCtrl', function ($scope, $modalInstance) {
 
   $scope.searchLiveStream = function (userName) {
-    searchLiveStream(userName);
+    searchLiveStream('twitch', userName);
   };
 
   $scope.modal_switchStream = function(){
@@ -63,29 +90,67 @@ StreamApp.controller('ModalInstanceCtrl', function ($scope, $modalInstance) {
 
 
 
-//Partie intéraction avec twitch
-function searchLiveStream(userName)
+
+function searchLiveStream(TypeOfStream, userName)
 {
-  $.getJSON('https://api.twitch.tv/kraken/users/'+userName+'/follows/channels?limit=50&offset=0&callback=?', function(data) {
 
-      follow_name = [];
-      for (var i = 0; i < data.follows.length; i++) 
-      {
-        follow_name.push(data.follows[i].channel.name);
-      }
+  /********************************************/
+  /******** Intéraction avec Twitch ***********/
+  /********************************************/
+  if (TypeOfStream == "twitch") 
+  {
 
-  }).done(function() {
+    $.getJSON('https://api.twitch.tv/kraken/users/'+userName+'/follows/channels?limit=50&offset=0&callback=?', function(data) {
 
-    //clear Online Stream
-    $('#emplacement_list_stream').html("");
+        follow_name = [];
+        for (var i = 0; i < data.follows.length; i++) 
+        {
+          follow_name.push(data.follows[i].channel.name);
+        }
 
-    //Fill Online Stream
-    WhichIsLive();
+    }).done(function() {
 
-  });
+      //clear Online Stream
+      $('#emplacement_list_stream_twitch').html("");
+
+      //Fill Online Stream
+      WhichTwitchIsLive();
+
+    });
+
+  }
+
+  /*************************************************/
+  /******** Intéraction avec Dailymotion ***********/
+  /*************************************************/
+  else if (TypeOfStream == "dailymotion")
+  {
+
+    $.getJSON('https://api.dailymotion.com/user/x1f124w/videos?fields=id,mode,onair,title,&private=0&sort=visited&limit=100', function(data) {
+
+        follow_name = [];
+        for (var i = 0; i < data.follows.length; i++) 
+        {
+          follow_name.push(data.follows[i].channel.name);
+        }
+
+    }).done(function() {
+
+      //clear Online Stream
+      $('#emplacement_list_stream_dailymotion').html("");
+
+      //Fill Online Stream
+      $('#emplacement_list_stream_dailymotion').append('<li class="list-group-item"><input type="radio" name="nom_chaine" value="'+follow_online[i][0]+'" id="'+follow_online[i][0]+'"><label for="'+follow_online[i][0]+'">'+follow_online[i][0]+'</label><span class="badge">'+follow_online[i][1]+'</span></li>');
+
+    });
+
+  }
+
+
 }
 
-function WhichIsLive()
+
+function WhichTwitchIsLive()
 {
   follow_online = [];
   for (var j = 0; j < follow_name.length; j++) 
@@ -106,7 +171,7 @@ function WhichIsLive()
     for (var i = 0; i < follow_online.length; i++) 
     {
  
-      $('#emplacement_list_stream').append('<li class="list-group-item"><input type="radio" name="nom_chaine" value="'+follow_online[i][0]+'" id="'+follow_online[i][0]+'"><label for="'+follow_online[i][0]+'">'+follow_online[i][0]+'</label><span class="badge">'+follow_online[i][1]+'</span></li>');
+      $('#emplacement_list_stream_twitch').append('<li class="list-group-item"><input type="radio" name="nom_chaine" value="'+follow_online[i][0]+'" id="'+follow_online[i][0]+'"><label for="'+follow_online[i][0]+'">'+follow_online[i][0]+'</label><span class="badge">'+follow_online[i][1]+'</span></li>');
     }
   });
 
